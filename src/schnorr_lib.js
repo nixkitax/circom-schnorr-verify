472aab79fde68a8b2347b42f24143b4cc002ffc0
thatsneekol <nemanuele@studenti.uninsubria.it>
2023-08-30 11:35:35 +0200

    Updates: Fixes and Miscellaneous Adjustments
    Add the possibility to create input.json, fixed index out of bounds, file existence, add a choose to generate circom json or not and other

--- src/schnorr_lib.js
+++ src/schnorr_lib.js
@@ -17,16 +17,42 @@ const { ArgumentParser } = require('argparse');
         parser.add_argument('-c', '--createSignature', { action: "store_true", help: 'creation of a signature', required: false});
         parser.add_argument('-i', '--index', { type: Number, help: 'index of a privateKey', required: false, default: 0});
         parser.add_argument('-m', '--message', { type: String, help: 'Message to sign', required: false, default: array_bytes_to_hex(crypto.randomBytes(32)) });
-        parser.add_argument('-v', '--verifySign', { action: "store_true", help: 'message to sign', required: false  });
+        parser.add_argument('-v', '--verifySign', { action: "store_true", help: 'verify s signature', required: false  });
         parser.add_argument('-p', '--pPubKey', { type: String, help: 'publicKey to verify a signature', required: false, default: " "  });
         parser.add_argument('-s', '--signature', { type: String, help: 'signature to verify', required: false, default: " "  });
         parser.add_argument('-orm', '--originalMsg', { type: String, help: 'original message to verify', required: false, default: " "  });
+        parser.add_argument('-circom', '--circomJSON', { action: "store_true", help: 'pin if you want json for circom', required: false  });
 
         const args = parser.parse_args();
         if (args.generateKeys) generate_keys(args.number);
-        if (args.createSignature){ 
-            const privateKey = await return_private_key(args.index);
-            sign_schnorr(args.message, privateKey, "sign");
+        if (args.createSignature) {
+            if (fs.existsSync('../json/users.json')) {
+                fs.readFile('../json/users.json', 'utf8', async (err, data) => {
+                if (err) {
+                    console.error("Errore nella lettura del file:", err);
+                    return;
+                }
+                try {
+                    const jsonData = JSON.parse(data);
+                    const numberOfPublicKeys = jsonData.users.length;
+                    if(args.index > numberOfPublicKeys){ 
+                        console.error("> Error in execution: Index (-i) is out of bounds, there are ", numberOfPublicKeys, " keys in users.json (first one -> 0)"); 
+                        process.exit(1);
+                    }
+                    const privateKey = await return_private_key(args.index);
+                    if(args.circomJSON) 
+                        sign_schnorr(args.message, privateKey, "signC");
+                    else 
+                        sign_schnorr(args.message, privateKey, "sign");
+                    
+                } catch (parseError) {
+                    console.error("Errore nel parsing del JSON:", parseError);
+                }
+                });
+            } else {
+                console.error(`> Error in execution: You have to generate your keys with: node schnorr_lib-js -g -n numKeys, look at https://github.com/lyylaaa/circom-schnorr-verify`);
+                process.exit(1);
+            }
         }
         if(args.verifySign){
             if(args.pPubKey == " ") console.error("You have to insert a compressed public key [HEX] to verify a signature, node schnorr.js -h");
@@ -40,6 +66,13 @@ const { ArgumentParser } = require('argparse');
         console.error("Si è verificato un errore:", error);
     }
 })();
+
+const x = (P) => byte_array_to_int(P[0]);
+
+const y = (P) => byte_array_to_int(P[1]);
+
+const has_even_y = (P) => y(P) % 2n == 0n;
+
 
 const array_bytes_to_hex = (bytes) => {
     return Array.from(bytes, (byte) => byte.toString(16).padStart(2, "0")).join(
@@ -55,10 +88,6 @@ const hex_to_array_bytes = (hexString) => {
     return bytes;
 };
 
-const x = (P) => byte_array_to_int(P[0]);
-
-const y = (P) => byte_array_to_int(P[1]);
-
 const byte_array_to_int = (byteArray) => {
     let bigIntValue = 0n;
     for (let i = 0; i < byteArray.length; i++) {
@@ -68,7 +97,6 @@ const byte_array_to_int = (byteArray) => {
     return bigIntValue;
 };
 
-const has_even_y = (P) => y(P) % 2n == 0n;
 
 const big_int_from_hex = (str) => BigInt('0x' + str);
 
@@ -133,7 +161,7 @@ const return_private_key = async (index) => {
     try {
         const data = await fsp.readFile('../json/users.json', 'utf8'); // Utilizza await per aspettare la lettura del file
         const jsonData = JSON.parse(data);
-        const privateKey = jsonData.users[0].privateKey;
+        const privateKey = jsonData.users[index].privateKey;
 
         return privateKey; // Restituisce la chiave privata
     } catch (err) {
@@ -160,26 +188,23 @@ const verify_key_pair = (msg, prvKeyHex) => {
     return sign_schnorr(msg, prvKeyHex, "verKey");
 }
 
-const update_users_json = (object, path) => {
+const update_json = (object, path) => {
     const jsonString = JSON.stringify(object, null, 2);
     fs.writeFileSync(path, jsonString);
 };
 
+
+
 const sign_schnorr = (msg, privateKey, type) => {
 
     d0 = hex_to_big_int(privateKey);
-
     if (d0 > babyJub.order - 1n)  throw new Error("prvKey has to be minor than order-1 ");
-
     let P = babyJub.mulPointEscalar(babyJub.Base8, d0);
-
     let d; //private key
-
     if (has_even_y(P)) 
         d = d0
     else 
         d = babyJub.order - d0;
-
     const nonceValue = crypto.randomBytes(32);
     const hashMsg = crypto
         .createHash("sha256")
@@ -189,13 +214,9 @@ const sign_schnorr = (msg, privateKey, type) => {
         .createHash("sha256")
         .update(hashMsg + nonceValue + msg)
         .digest("hex");
-
     const k0 = (hex_to_big_int(combinedHash))% babyJub.order;
-
     const r = babyJub.mulPointEscalar(babyJub.Base8, k0);
-
     const concatHash = hex_from_big_int(x(r)) + hex_from_big_int(x(P)) + msg;
-
     const e = hex_to_big_int(crypto
          .createHash("sha256")
          .update(concatHash)
@@ -210,17 +231,54 @@ const sign_schnorr = (msg, privateKey, type) => {
             return verify_signature(pPubKey, msg, signature, "verKey");
         case "sign": 
             console.log("");
-            console.log("> [Sign_Schnorr] d:                       ", d);
-            console.log("> [Sign_Schnorr] k                        ", k0);
-            console.log("> [Sign_Schnorr] e:                       ", e);
-            console.log("> [Sign_Schnorr] x(r):                    ", x(r));
-            console.log("> [Sign_Schnorr] n:                       ", babyJub.order);
+            console.log("> \x1b[32m[Sign_Schnorr] \x1b[0md:            ", d);
+            console.log("> \x1b[32m[Sign_Schnorr] \x1b[0mk             ", k0);
+            console.log("> \x1b[32m[Sign_Schnorr] \x1b[0me:            ", e);
+            console.log("> \x1b[32m[Sign_Schnorr] \x1b[0mx(r):         ", x(r));
+            console.log("> \x1b[32m[Sign_Schnorr] \x1b[0mn:            ", babyJub.order);
             console.log("");
-            console.log("> [Sign_Schnorr][LSign][RSign] (bigInt): ", "\n\n\t   [",big_int_from_hex(LSign),"]\n\t   [",big_int_from_hex(RSign),"]\n");
-            console.log("> [Sign_Schnorr]([LSign][RSign]) (HEX): ", "\n\n\t   [",LSign + RSign,"]\n");
-            console.log("> [Sign_Schnorr] message:      ", msg);
-            console.log("> [Sign_Schnorr] public key:   ", array_bytes_to_hex(pPubKey) )
+            console.log("> \x1b[32m[Sign_Schnorr] \x1b[0m[LSign][RSign] (bigInt): ", "\n\n\t   [",big_int_from_hex(LSign),"]\n\t   [",big_int_from_hex(RSign),"]\n");
+            console.log("> \x1b[32m[Sign_Schnorr] \x1b[0m([LSign][RSign]) (HEX): ", "\n\n\t   [",LSign + RSign,"]\n");
+            console.log("> \x1b[32m[Sign_Schnorr] \x1b[0mmessage:      ", msg);
+            console.log("> \x1b[32m[Sign_Schnorr]\x1b[0m publicKey:    ", array_bytes_to_hex(pPubKey));
+            console.log("> \x1b[32m[Sign_Schnorr]\x1b[0m privateKey:   ", privateKey);
+
+            var isOK = verify_signature(pPubKey, msg, signature, "verKey");
+            if(isOK) {
+                console.log("> \x1b[32m[Sign_Schnorr]\x1b[0m Sign status:   \x1b[32mokay\x1b[0m ");
+            }
+            else console.log("> \x1b[32m[Sign_Schnorr]\x1b[0m Sign status:   \x1b[31mnot okay\x1b[0m");
             console.log("");
+            //console.log( hex_to_array_bytes(array_bytes_to_hex(pPubKey)))
+            //return verify_signature(pPubKey, msg, signature, "verify");
+            break;
+        case "signC":
+            console.log("");
+            console.log("> \x1b[32m[Sign_Schnorr] \x1b[0md:            ", d);
+            console.log("> \x1b[32m[Sign_Schnorr] \x1b[0mk             ", k0);
+            console.log("> \x1b[32m[Sign_Schnorr] \x1b[0me:            ", e);
+            console.log("> \x1b[32m[Sign_Schnorr] \x1b[0mx(r):         ", x(r));
+            console.log("> \x1b[32m[Sign_Schnorr] \x1b[0mn:            ", babyJub.order);
+            console.log("");
+            console.log("> \x1b[32m[Sign_Schnorr] \x1b[0m[LSign][RSign] (bigInt): ", "\n\n\t   [",big_int_from_hex(LSign),"]\n\t   [",big_int_from_hex(RSign),"]\n");
+            console.log("> \x1b[32m[Sign_Schnorr] \x1b[0m([LSign][RSign]) (HEX): ", "\n\n\t   [",LSign + RSign,"]\n");
+            console.log("> \x1b[32m[Sign_Schnorr] \x1b[0mmessage:      ", msg);
+            console.log("> \x1b[32m[Sign_Schnorr]\x1b[0m publicKey:    ", array_bytes_to_hex(pPubKey));
+            console.log("> \x1b[32m[Sign_Schnorr]\x1b[0m privateKey:   ", privateKey);
+
+            var isOK = verify_signature(pPubKey, msg, signature, "verKey");
+            if(isOK) {
+                console.log("> \x1b[32m[Sign_Schnorr]\x1b[0m Sign status:   \x1b[32mokay\x1b[0m ");
+                const jsonObject = {
+                    "LSign" : signature.slice(0, 64),
+                    "RSign" : signature.slice(64),
+                    "msg" : msg,
+                    "pPub" : array_bytes_to_hex(pPubKey)
+                }
+                update_json(jsonObject, "../json/input.json");
+                console.log("\n> \x1b[32m[Sign_Schnorr]\x1b[0m Created\x1b[34m input.json \x1b[0mfor circom!");
+            }
+            else console.log("> \x1b[32m[Sign_Schnorr]\x1b[0m Sign status:   \x1b[31mnot okay\x1b[0m");
             //console.log( hex_to_array_bytes(array_bytes_to_hex(pPubKey)))
             //return verify_signature(pPubKey, msg, signature, "verify");
             break;
@@ -235,7 +293,6 @@ const verify_signature = (pPubKey, msg, signature, type) => {
     if(type == "verify")
         pPubKey = hex_to_array_bytes(pPubKey);
 
-    //console.log("msg", msg);
     let P = babyJub.unpackPoint(pPubKey);
     let isOK;
     const LSign = signature.slice(0, 64);
@@ -263,13 +320,13 @@ const verify_signature = (pPubKey, msg, signature, type) => {
                 if(isOK) console.log("\n\t\t\t\t\t  Verification is OK :)");
                 break;
             case "default":
-                console.error("There is unknown paramaters for schnorr_sign");
+                console.error("There is unknown paramaters for verify_signature");
     }
 }
 
 const generate_keys = (numKeys) => {
 
-    console.log("> [Generation Key] Generating","[", numKeys,"]", "in \"../json/users.json");
+    console.log("> \x1b[32m[Generation Key] \x1b[0mGenerating","[", numKeys,"] keys in \"../json/users.json");
     let count = 0;
     let object = {
         $schema: "./users_schema.json",
@@ -298,14 +355,6 @@ const generate_keys = (numKeys) => {
             }
     }
     
-    console.log("> [Generation Key] Created","   [", numKeys,"]", "in \"../json/users.json");
-    update_users_json(object, "../json/users.json");
-};
-
-
-
-
-module.exports = {
-    generate_keys,
-    //initializeBabyJub
+    console.log("> \x1b[32m[Generation Key] \x1b[0mGenerating [", numKeys,"] keys in \"../json/users.json");
+    update_json(object, "../json/users.json");
 };
